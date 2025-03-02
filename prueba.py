f58942460cff1752a6e43697f5b1579fbb414c42
Victor Mosquera <victormanuelmosqueraconejo@gmail.com>
2025-03-02 17:26:33 -0500

    Commit de prueba 2

--- prueba.py
+++ prueba.py
@@ -3,7 +3,7 @@ import random as rd
 
 adivinar = rd.int()
 
-numero = input("Adivina el numero del 1 al 6:")
+numero = input(int("Adivina el numero del 1 al 6:"))
 
 if (numero != adivinar):
     print("Para la proxima")
